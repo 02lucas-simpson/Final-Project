982e48207758503ae988505b465050783edbb8d6
Lucas Simspon <02lucas.simpson@gulllakecs.org>
2019-03-11 09:55:21 -0400

    defined all variables

---
+++ Mad libs v1.py
@@ -0,0 +1,23 @@
+# Lucas Simpson
+# 3/11/19
+
+adjective = input('Choose an adjective: ')
+adjective2 = input('Choose an adjective: ')
+noun = input('Choose a noun: ')
+noun2 = input('Choose a noun: ')
+plural_noun = input('Choose a plural noun: ')
+game = input('Choose a game: ')
+plural_noun2 = input('Choose a plural noun: ')
+verb = input('Choose a verb ending in "ing: "')
+verb2 = input('Choose a verb ending in "ing: "')
+plural_noun3 = input('Choose a plural noun: ')
+verb3 = input('Choose a verb ending in "ing: "')
+noun3 = input('Choose a noun: ')
+plant = input('Choose a plant: ')
+body_part = input('choose a part of the body: ')
+place = input('Choose a place: ')
+verb4 = input('Choose a verb ending in "ing: "')
+adjective3 = input('Choose an adjective: ')
+number = input(float('Choose a number: '))
+plural_noun4 = input('Choose a plural noun: ')
+
