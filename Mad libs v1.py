3146593e29a1cc026c7caedadb6233d44c10dd32
Lucas Simspon <02lucas.simpson@gulllakecs.org>
2019-03-12 09:49:39 -0400

    Defined all variables and added a function

--- Mad libs v1.py
+++ Mad libs v1.py
@@ -1,6 +1,19 @@
 # Lucas Simpson
 # 3/11/19
 
+#added a nice greating
+
+name = input("What is your name: ")
+
+def greeting():
+    print ("Hi there " + name + "!")
+    print ("Nice to meet you!")
+
+greeting()
+
+
+# Declared all variables
+
 adjective = input('Choose an adjective: ')
 adjective2 = input('Choose an adjective: ')
 noun = input('Choose a noun: ')
@@ -8,16 +21,17 @@ noun2 = input('Choose a noun: ')
 plural_noun = input('Choose a plural noun: ')
 game = input('Choose a game: ')
 plural_noun2 = input('Choose a plural noun: ')
-verb = input('Choose a verb ending in "ing: "')
-verb2 = input('Choose a verb ending in "ing: "')
+verb = input('Choose a verb ending in "ing": ')
+verb2 = input('Choose a verb ending in "ing": ')
 plural_noun3 = input('Choose a plural noun: ')
-verb3 = input('Choose a verb ending in "ing: "')
+verb3 = input('Choose a verb ending in "ing": ')
 noun3 = input('Choose a noun: ')
 plant = input('Choose a plant: ')
 body_part = input('choose a part of the body: ')
 place = input('Choose a place: ')
-verb4 = input('Choose a verb ending in "ing: "')
+verb4 = input('Choose a verb ending in "ing": ')
 adjective3 = input('Choose an adjective: ')
-number = input(float('Choose a number: '))
+number = input('Choose a number: ')
 plural_noun4 = input('Choose a plural noun: ')
+
 
